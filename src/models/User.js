0d4667fb647a1e422ff68afede975da862e5662a
Ruan Chagas <ruanchagas95@gmail.com>
2024-01-26 15:44:37 -0300

    feat: create UserModel

---
+++ src/models/User.js
@@ -0,0 +1,29 @@
+const UserModel = (sequelize, DataTypes) => {
+  const User = sequelize.define("User", {
+    id: {
+      type: DataTypes.INTEGER,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    display_name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    email: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      unique: true,
+    },
+    password: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    image: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+  });
+  return User;
+};
+
+module.exports = UserModel;
