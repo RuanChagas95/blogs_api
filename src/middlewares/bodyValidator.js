ad5ed0a8f9a42eb62017e953609f5e9f0c4b3bea
Ruan Chagas <ruanchagas95@gmail.com>
2024-01-31 16:39:47 -0300

    fix: bodyValidator crash

--- src/middlewares/bodyValidator.js
+++ src/middlewares/bodyValidator.js
@@ -2,7 +2,7 @@ const validators = require('../validators');
 
 function bodyValidator(req, _res, next) {
   const result = validators[`${req.baseUrl}@${req.method}`].validate(req.body);
-  if (result) throw new Error(`400|${result.error.message}`);
+  if (result.error) throw new Error(`400|${result.error.message}`);
   next();
 }
 
