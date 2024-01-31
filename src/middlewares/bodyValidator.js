e4a3d8da6f6132b7a5d747871f23e9a9da61dbd4
Ruan Chagas <ruanchagas95@gmail.com>
2024-01-31 16:13:23 -0300

    refact: bodyValidator

--- src/middlewares/bodyValidator.js
+++ src/middlewares/bodyValidator.js
@@ -1,8 +1,8 @@
 const validators = require('../validators');
 
 function bodyValidator(req, _res, next) {
-  const { error } = validators[`${req.baseUrl}@${req.method}`].validate(req.body);
-  if (error) throw new Error(`400|${error.message}`);
+  const result = validators[`${req.baseUrl}@${req.method}`].validate(req.body);
+  if (result) throw new Error(`400|${result.error.message}`);
   next();
 }
 
